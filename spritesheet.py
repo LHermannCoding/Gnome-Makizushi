1590702e2216a19be563f958a81e8c56003438cd
Luke Hermann <lukehermann@uchicago.edu>
2023-08-11 21:34:02 -0500

    finished header docs

--- spritesheet.py
+++ spritesheet.py
@@ -1,10 +1,18 @@
 import pygame
 
 class SpriteSheet():
+    """
+    Class for managing gnome spritesheets to reduce asset load required
+    and use animation swaps.
+    """
     def __init__(self, image):
         self.sheet = image
         
     def get_image(self,frame,width,height,color,scale=1):
+        """
+        Get specific image from spritesheet based on given frame.
+
+        """
         image = pygame.Surface((width,height)).convert_alpha()
         image.blit(self.sheet,(0,0), ((frame * width), 0, width, height))
         image = pygame.transform.scale(image, (width * scale, height * scale))
